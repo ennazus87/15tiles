a193fbae6e41944f89f38b2bda150b05fbf7869d
Suzanne Raine <suzanne.raine@wyoming-interactive.com>
2018-09-05 14:10:10 +0100

    Latest code

--- Board.js
+++ Board.js
@@ -1,16 +1,18 @@
-//stops multiple boards being displayed
-let scriptAdded = false;
-
 class Board {
-
     constructor(r, c) {
         this.row = r;
         this.col = c;        
         
         this.gameBoard = new Array(this.row); // array for gameboard
+        this.original = new Array();
+
         this.blank = { x: this.col-1, y: this.row-1 } // stores position of blank tile
+
+        //stops multiple boards being displayed
+        this.scriptAdded = false;
     }
 
+    //creates the 2d array depending on the number of rows/cols that the user has specified
     createBoardArray() {
         let count = 1;
     
@@ -28,8 +30,13 @@ class Board {
                 count++;
             }
         }
+
+    //record original
+    //     this.original = Object.assign({}, this.gameBoard)
+    //     console.log(this.original)
     }
 
+    //displays board
     renderBoard() {
         let table = document.createElement("table");
         table.id = "game-table";
@@ -56,12 +63,13 @@ class Board {
             table.appendChild(tr);
         }
         
-        if(scriptAdded == false) {
+        if(this.scriptAdded == false) {
             document.getElementById('container').appendChild(table);
-            scriptAdded = true;
+            this.scriptAdded = true;
         }
     }
 
+    //the moves
     moveRight() {
         if (this.blank.x !== 0) {
             const temp = this.gameBoard[this.blank.y][this.blank.x-1] // store the value of the tile the blank is being swapped with
@@ -109,32 +117,67 @@ class Board {
         
     }
 
+    //avoids the game board from being rendered each time
     resetScript() {
-        scriptAdded = false;
+        this.scriptAdded = false;
         this.renderBoard()
+        
+        let elem = document.getElementById("game-table");
+        elem.parentNode.removeChild(elem);
     }
 
+    //to ensure the board CAN be completed, it is first placed in order, then is shuffled
+    //this shuffle is optimised to ensure that even really large boards are shuffled. 
     shuffle() {
-        for (let i = 0; i < 2500; i++) {
-            let randomInt = Math.floor(Math.random() * 4) + 1 ; 
+        let numberOfShuffles = (Math.pow(this.row, 2) * (Math.pow(this.col, 2)));
+        console.log(numberOfShuffles);
+
+        //optimising shuffle
+        let banned = 3; 
+        let randomInt = 3;
+
+        for (let i = 0; i < numberOfShuffles; i++) {
+            do {
+                randomInt = Math.floor(Math.random() * 4) + 1 ; 
+            } while (banned === randomInt || i === 0 && randomInt === 4)
 
             switch (randomInt) {
                 case 1:
                     this.moveLeft();
+                    banned = 3;
                     break;
                 case 2:
                     this.moveUp();
+                    banned = 4;
                     break;
                 case 3:
                     this.moveRight();
+                    banned = 1;
                     break;
                 case 4:
                     this.moveDown();
+                    banned = 2
                     break;
             }
         }
     }
 
+    //to know if something is shuffled, it should not still be in it's original position.
+    // isShuffled() {
+    //     let shuffled = new Array();
+
+    //     // for (let i = 0; i < this.gameBoard.length; i++) {
+    //     //     for (let j = 0; j < this.gameBoard[i].length; j++) {           
+    //     //         shuffled.push(this.gameBoard[i][j])
+    //     //     }
+    //     // }
+
+    //     // console.log("o: " + this.original)
+    //     console.log("s: " + shuffled);
+    // }
+
+    //checks if the board array matches the winning array
+    //i.e. if the player is a winner or not. 
     validate() {
         let board = new Array();
         let winner = new Array();
@@ -148,16 +191,19 @@ class Board {
         winner = board.slice(0)
         winner.sort(function(a, b) {
             return a - b;
-          });
+        });
 
         winner = winner.filter(function(e) { return e !== 0 })
         winner.push(0)
 
-        console.log('board: ' + board);
-        console.log('winner: ' + winner);
+        // console.log('board: ' + board);
+        // console.log('winner: ' + winner);
 
-        if (board.length === winner.length && board.every((value, index) => value === winner[index])) {
-            console.log("winner winner chicken dinner")
+        if (board.length === winner.length && board.every((value, index) => value === winner[index])) {  
+            setTimeout(function() { 
+                document.getElementById('game-table').innerHTML = "WELL PLAYED!";
+                console.log("winner winner chicken dinner") 
+            }, 1000);
         } else {
             console.log("not a winner")
         }
